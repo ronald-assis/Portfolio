156c3313107566a6433d38e64ba24eddc4910632
Ronald Assis <ronaldassis71@gmail.com>
2021-12-07 17:30:50 -0300

    applying header in portffolio

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,1 +1,15 @@
 import React from 'react';
+import Logo from './Logo';
+import Navigation from './Navigation';
+import './styles/header.css';
+
+function Header () {
+  return (
+    <header>
+      <Logo />
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
