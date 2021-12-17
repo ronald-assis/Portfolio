69ef44d88b56b2e9df66d92c7e09b59db9634c03
Ronald Assis <ronaldassis71@gmail.com>
2021-12-17 14:03:56 -0300

    Add information of the projects

---
+++ src/Data/index.js
@@ -0,0 +1,24 @@
+const projects = [
+  {
+    title: "projeto 1",
+    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse similique mollitia possimus at accusantium, quis nam expedita eos modi excepturi amet provident sequi, corrupti eveniet minus? Ipsa quae in repellendus!",
+    languages: ["React", "JavaScript"],
+  },
+  {
+    title: "projeto 2",
+    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse similique mollitia possimus at accusantium, quis nam expedita eos modi excepturi amet provident sequi, corrupti eveniet minus? Ipsa quae in repellendus!",
+    languages: ["React", "JavaScript", "NodeJS"],
+  },
+  {
+    title: "projeto 3",
+    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse similique mollitia possimus at accusantium, quis nam expedita eos modi excepturi amet provident sequi, corrupti eveniet minus? Ipsa quae in repellendus!",
+    languages: ["JavaScript", "CSS"],
+  },
+  {
+    title: "projeto 4",
+    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse similique mollitia possimus at accusantium, quis nam expedita eos modi excepturi amet provident sequi, corrupti eveniet minus? Ipsa quae in repellendus!",
+    languages: ["React", "JavaScript"],
+  },
+];
+
+export default projects;
