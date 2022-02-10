2f0b1cbb2ba5b202a279547afe9c427d7a1571db
Ronald Assis <ronaldassis71@gmail.com>
2022-02-10 19:16:41 -0300

    file remove provider and adding scroll behavior

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PortfolioProvider from "./context";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import "./Global.css";
@@ -7,10 +6,8 @@ import "./Global.css";
 function App() {
   return (
     <div className="App">
-      <PortfolioProvider>
-        <Header />
-        <Main />
-      </PortfolioProvider>
+      <Header />
+      <Main />
     </div>
   );
 }
