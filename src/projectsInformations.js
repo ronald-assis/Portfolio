2e4438ce7489d42070897565450da11a832989ba
ronald <ronaldassis71@gmail.com>
2022-03-03 17:28:13 -0300

    Adding info of project Rocket.q

--- src/projectsInformations.js
+++ src/projectsInformations.js
@@ -1,12 +1,13 @@
 import maskGroup from "./components/images/mask_group.png";
 import trybeTunesImg from './components/images/trybeTunes.png';
+import illustration from './components/images/illustration.png';
 
 const projects = [
   {
-    title: "projeto 1",
-    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse similique mollitia possimus at accusantium, quis nam expedita eos modi excepturi amet provident sequi, corrupti eveniet minus? Ipsa quae in repellendus!",
-    image: maskGroup,
-    languages: ["React", "JavaScript", "NodeJS"],
+    title: "Rocket.Q",
+    info: "O Rocket.Q é uma aplicação de interação através de perguntas, sendo possível criar uma sala para internautas anônimos fazerem perguntas e o criador da sala em posse de uma senha gerenciar essas perguntas e marcar como lidas.",
+    image: illustration,
+    languages: ["JavaScript","CSS", "NodeJS"],
   },
   {
     title: "Trybe Tunes",
