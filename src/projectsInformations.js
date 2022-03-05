eac567611d517f8ad70c5fc4640cbcfe3e140a6a
ronald <ronaldassis71@gmail.com>
2022-03-05 15:07:43 -0300

    finish projects info

--- src/projectsInformations.js
+++ src/projectsInformations.js
@@ -1,6 +1,8 @@
-import maskGroup from "./components/images/mask_group.png";
+// import maskGroup from "./components/images/mask_group.png";
 import trybeTunesImg from './components/images/trybeTunes.png';
 import illustration from './components/images/illustration.png';
+import coverImage from './components/images/cover_image.png';
+import trybeShopping from './components/images/trybe_shopping.png';
 
 const projects = [
   {
@@ -16,16 +18,16 @@ const projects = [
     languages: ["React", "JavaScript", "CSS"],
   },
   {
-    title: "projeto 3",
-    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse similique mollitia possimus at accusantium, quis nam expedita eos modi excepturi amet provident sequi, corrupti eveniet minus? Ipsa quae in repellendus!",
-    image: maskGroup,
-    languages: ["JavaScript", "CSS"],
+    title: "Book synopsis",
+    info: "Um projeto onde Login e acesso de Usuário já registrado, ao fazer o login o app faz uma requisição API que retorna uma listagem de livros, e ao cliclar no livro escolhido mostra os detalhes e a sinopse.",
+    image: coverImage,
+    languages: ["React", "CSS"],
   },
   {
-    title: "projeto 4",
-    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse similique mollitia possimus at accusantium, quis nam expedita eos modi excepturi amet provident sequi, corrupti eveniet minus? Ipsa quae in repellendus!",
-    image: maskGroup,
-    languages: ["React", "JavaScript"],
+    title: "Shopping Card",
+    info: "Nesse projeto terá um carrinho de compras totalmente dinâmico! Através de requisições HTTP. Aqui usarei a API do Mercado Livre para buscar produtos à venda.",
+    image: trybeShopping,
+    languages: ["JavaScript", "HTML", "CSS"],
   },
 ];
 
